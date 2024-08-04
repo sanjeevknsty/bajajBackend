9d7f49064e5fca2b81fd74956ab96c2fbfd394f6
sanjeevknsty <ksanjeev0359@gmail.com>
2024-08-05 00:28:29 +0530

    changes

--- fetch.js
+++ fetch.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router()
 const { body, validationResult } = require('express-validator')
 
+
+// const {is_success,user_id,email,alphabets,numbers,roll_number}  = result 
 router.get('/bfhl', async (req, res) => {
 
   try {
@@ -17,20 +19,22 @@ router.get('/bfhl', async (req, res) => {
   }
 })
 
-router.post('/bfhl',
-  // [
-  //   body('data', 'Enter data'),
-  // ], 
-  async (req, res) => {
+router.post('/bfhl', async (req, res) => {
+  var result = {
+    is_success : false,
+    user_id: "sanjeev_konisetty_16072004",
+    email: "kk7140@srmist.edu.in",
+    roll_number: "RA2111027020055",
+  }
     const { data } = req.body
     console.log(data)
     try {
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
-        res.status(400).send({ errors: errors.array() })
+        // is_success = false
+        res.status(400).json({is_success,user_id,email,roll_number,errorMessage:errors.array()})
+        // send({ errors: errors.array(),result })
       }
-      // const data = JSON.parse({data1})
-      // console.log(data)
       const filterNumber = data.filter(str => !isNaN(str))
       const filterString = data.filter(str => /^[a-zA-Z]+$/.test(str))
 
@@ -38,27 +42,39 @@ router.post('/bfhl',
         .filter(str => /^[a-zA-Z]+$/.test(str))  // Filter only alphabetic strings
         .reduce((a, b) => a > b ? a : b) : [];
 
-      const result = {
-        is_success: !res.ok ? true : false,
-        user_id: "sanjeev_konisetty_16072004",
-        email: "kk7140@srmist.edu.in",
-        roll_number: "RA2111027020055",
-        numbers: filterNumber.length ? filterNumber : [],
-        alphabets: filterString.length ? filterString : [],
-        highest_alphabet: highestAlphabetical,
-      }
-      if (result.alphabets.length > 0) {
-        for (let i = 0; i < result.alphabets.length; i++) {
-          if (result.alphabets[i].length > 1) {  
+      // result = {
+      //   is_success: !res.ok ? true : false,
+        // user_id: "sanjeev_konisetty_16072004",
+        // email: "kk7140@srmist.edu.in",
+        // roll_number: "RA2111027020055",
+      //   numbers: filterNumber.length ? filterNumber : [],
+      //   alphabets: filterString.length ? filterString : [],
+      //   highest_alphabet: highestAlphabetical,
+      // }
+      
+    
+        result.numbers =filterNumber.length ? filterNumber : []
+        result.alphabets = filterString.length ? filterString : []
+        result.highest_alphabet =  highestAlphabetical
+
+        const {is_success,user_id,email,roll_number,alphabets} = result
+      
+      if (alphabets.length > 0) {
+        for (let i = 0; i < alphabets.length; i++) {
+          if (alphabets[i].length > 1) {  
             result.message = "Alphabet should be single character" 
-            return res.status(500).json({ message: "Alphabet should be single character" })
+            // result.is_success = false
+            return res.status(500).json({is_success,user_id,email,roll_number,message:"Alphabet should be single character"})
           }
         }
       }
+      result.is_success = true
       res.json(result)
     } catch (error) {
-      res.status(500).send(error.message)
-      // console.error(error.message)
+      const {is_success,user_id,email,roll_number} = result
+      // result.is_success = false
+      res.status(500).send({is_success,user_id,email,roll_number,errorMessage:error.message})
+      console.error(error.message)
     }
 
 
